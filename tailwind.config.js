cb9adbeb9f399804630ffdfb11aa928a3f18e58d
ArtisGulbis <55058842+ArtisGulbis@users.noreply.github.com>
2021-08-09 14:20:27 +0200

    adjusted twcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-    './src/components/*.{js,jsx,ts,tsx}',
-    './src/*.{js,jsx,ts,tsx}',
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
